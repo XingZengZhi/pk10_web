e359144e1bbc9362bc554b2a8ec92a2bdc371870
xingzengzhi <xingzengzhi@qq.com>
2018-09-06 01:10:35 +0800

    增加首页

--- js/login.js
+++ js/login.js
@@ -1,4 +1,24 @@
 window.onload = function(){
+    var _login = function() {
+        layui.use('layer', function(){
+            var layer = layui.layer;
+            if(userName.value === '' || userPass === '') {
+                layer.msg("账户或密码不能为空！");
+            } else {
+                if(userName.value.toLowerCase() === userData.userName &&
+                        userPass.value.toLowerCase() === userData.userPass) {
+                    var loadIndex = layer.load(1);
+                    setTimeout(function(){
+                        layer.close(loadIndex);
+                        location.href = "/view/main.html";
+                    }, 1000);
+                    // 后台登录操作
+                } else {
+                    layer.msg("登录失败，请检查账户或密码");
+                }
+            }
+        });
+    }
     // 模拟用户登录数据
     var userData = {
         userName:"admin",
@@ -19,19 +39,12 @@ window.onload = function(){
         loginSubmit = getDocElem("#loginSumbit");
     // 登录操作
     loginSubmit.onclick = function() {
-        layui.use('layer', function(){
-            var layer = layui.layer;
-            if(userName.value === '' || userPass === '') {
-                layer.msg("账户或密码不能为空！");
-            } else {
-                if(userName.value.toLowerCase() === userData.userName &&
-                        userPass.value.toLowerCase() === userData.userPass) {
-                    layer.load(1);
-                    // 后台登录操作
-                } else {
-                    layer.msg("登录失败，请检查账户或密码");
-                }
-            }
-        });
+        _login();
+    }
+    // 键盘操作
+    userPass.onkeypress = function(e){
+        if(e.key === "Enter"){
+            _login();
+        }
     }
 }
---
+++ js/main.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    // 侧边动态
+    // var leftBar = document.getElementById("leftBar"),
+    //     mainContent = document.getElementById("main_content");
+    //     navlist = document.getElementById("navlist");
+    // leftBar.onclick = function() {
+    //     var isShow = this.getAttribute("data-isshow");
+    //     if(isShow === 'false') {
+    //         navlist.style.transform = "translateX(250px)";
+    //         mainContent.style.marginLeft = "250px";
+    //         this.dataset.isshow = 'true';
+    //     } else {
+    //         navlist.style.transform = "translateX(-250px)";
+    //         mainContent.style.marginLeft = "0px";
+    //         this.dataset.isshow = 'false';
+    //     }
+    // }
+    
+}
