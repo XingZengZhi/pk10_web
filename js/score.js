d2105a3f3d4ff1fbb0711735348eeff3735dfdc6
xingzengzhi <xingzengzhi@qq.com>
2018-09-15 02:01:31 +0800

    优化挂卖积分页面

--- js/score.js
+++ js/score.js
@@ -5,4 +5,10 @@
     $(".close_detail").on("click", function(){
         $(".hang_detail").fadeOut(.3);
     });
+    $("#confirm_hang").on("click", function(){
+        $(".hang_detail2").fadeIn(.3);
+    });
+    $(".close_detail2").on("click", function(){
+        $(".hang_detail2").fadeOut(.3);
+    });
 })(window, document);
