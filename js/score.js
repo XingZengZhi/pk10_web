715139c8e6e6a2ee031a0f3014c42fa73c934aae
xingzengzhi <xingzengzhi@qq.com>
2018-09-15 18:44:32 +0800

    页面优化

--- js/score.js
+++ js/score.js
@@ -5,6 +5,7 @@
     $(".close_detail").on("click", function(){
         $(".hang_detail").fadeOut(.3);
     });
+
     $("#confirm_hang").on("click", function(){
         $(".hang_detail2").fadeIn(.3);
     });
@@ -16,5 +17,18 @@
     $("#secrity_code").on("click", function(){
         $(".code_tip").show(0);
         $("#input_code").hide(0);
+    });
+
+    $("#confirm_hang2").on("click", function(){
+        $(".hang_detail3").fadeIn(.3);
+    });
+    $(".close_detail3").on("click", function(){
+        $(".hang_detail3").fadeOut(.3);
+        $(".code_tip2").hide(0);
+        $("#input_code2").show(0);
+    });
+    $("#secrity_code2").on("click", function(){
+        $(".code_tip2").show(0);
+        $("#input_code2").hide(0);
     });
 })(window, document);
